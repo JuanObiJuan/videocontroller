0b0f999e1b82e02cb2240a26f551efe8ce838c8c
JuanObiJuan <obi_juan_@hotmail.com>
2020-05-11 21:44:35 +0200

    configuration updater feature

--- videoservice.py
+++ videoservice.py
@@ -1,5 +1,5 @@
 import json
-
+import urllib
 #The json format
   #"version": 202005111739,
   #"updateConfigUrl": "https://mydomain/config.json",
@@ -13,6 +13,18 @@ import json
 
 
 
-with open('config.json') as json_file:
-    config = json.load(json_file)
-    print (config[version])
+with open('/home/pi/config.json') as json_file:
+    localConfig = json.load(json_file)
+
+#TODO take care of no internet connection
+json_url_content= urllib.urlopen(localConfig["updateConfigUrl"])
+remoteConfig = json.load(json_url_content)
+
+if(remoteConfig["version"]>localConfig["version"]):
+  print("update videos and run mus.py")
+  #download new videos
+  #delete the old ones
+  #update the local config file
+  #run mus.py
+else
+  #run mus.py
