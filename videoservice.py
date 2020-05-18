c1313b6c913358f9524b8b4b41e6f055b6e19174
JuanObiJuan <obi_juan_@hotmail.com>
2020-05-18 20:59:13 +0200

    Feedback on console and in the interface. Fot this there is a new dependency (check the wiki)

--- videoservice.py
+++ videoservice.py
@@ -1,5 +1,6 @@
 import json
 import urllib.request
+import os
 #The json format
   #"version": 202005111739,
   #"updateConfigUrl": "https://mydomain/config.json",
@@ -11,34 +12,39 @@ import urllib.request
   #"loopMainVideoWhileUserInFront": true,
   #"mainVideoTimer": -1
 
-
-
-localConfigFile = "/home/pi/config.json"
-
+localConfigFile = "/home/pi/Repository/videocontroller/config.json"
 
 with open(localConfigFile) as json_file:
     localConfig = json.load(json_file)
-
+    print("local config loaded")
 # if the url can't be opened set "remoteConfig = localConfig"
 try:
     json_url_content= urllib.request.urlopen(localConfig["updateConfigUrl"])
     remoteConfig = json.load(json_url_content)
+    print("remote config loaded")
 except:
     remoteConfig = localConfig
+    print("No access to remote config")
+    os.system('notify-send "no access" "the device has no acces to check new updates"')
 
 if(remoteConfig["version"]>localConfig["version"]):
   #download new videos
+  print("new videos!")
+  os.system('notify-send "updating" "...downloading new videos"')
   try:
     urllib.request.urlretrieve(remoteConfig["introVideoUrl"],r"/home/pi/introvideo_new.mp4")
     urllib.request.urlretrieve(remoteConfig["mainVideoUrl"],r"/home/pi/mainvideo_new.mp4")
   except:
     print("could not download new video files")
+    os.system('notify-send "error" "Something went wrong updating the videos"')
   else:
     print("video update complete")
+    os.system('notify-send "ok" "Uupdate was successfull"')
     # overwrite old videos
     os.rename("/home/pi/introvideo_new.mp4","/home/pi/introvideo.mp4")
     os.rename("/home/pi/mainvideo_new.mp4","/home/pi/mainvideo.mp4")
     #update the local config file
     json.dump(remoteConfig ,open(localConfigFile,"w"),indent=2)
-
-#run mus.py
+else:
+  os.system('notify-send "loading" "Loading player"')
+  os.system('python3 /home/pi/Repository/videocontroller/mus.py')
